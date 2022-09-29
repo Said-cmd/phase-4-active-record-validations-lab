28054ad99f079612517f69203782ae70f27d1a70
Said Hussein <saidhussein12342@gmail.com>
2022-09-29 23:03:39 +0300

    commit tree 6d36b7ba7fa65127f3ddf14e810ba1ccaf6a30c9

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+    validate :reject_true_facts
+
+    def reject_true_facts
+        if title == "True Facts"
+        errors.add(:title, "Sorry the title can't be true facts")
+        end
+    end
+
 end
